f30b61894ebc61845a8402885f88197d71f8fa56
Shara <shara.shahverdian@gmail.com>
2024-09-25 02:38:44 +0330

    - update website - fix 404 not found page

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -2,7 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import store from "../redux/store/store.ts";
 import { Provider } from "react-redux";
 import NotFound from "./pages/NotFound.tsx";
@@ -13,7 +18,8 @@ createRoot(document.getElementById("root")!).render(
       <Router basename="/Shara-Sh/">
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="404" />} />
         </Routes>
       </Router>
     </Provider>
